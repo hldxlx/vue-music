0e2b536b9ca4f6f85f2f2e921c0be20f463823ac
斌斌斌 <1587443838@qq.com>
2019-04-14 07:41:34 +0800

    first commit

---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="m-header">
+    <div class="icon"></div>
+    <h1 class="text">Chicken Music</h1>
+    <!--<router-link tag="div" class="mine">&lt;!&ndash;to="/user"&ndash;&gt;-->
+      <!--<i class="icon-mine"></i>-->
+    <!--</router-link>-->
+    8989
+  </div>
+
+</template>
+
+<script type="text/ecmascript-6">
+  export default {}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+ </style>
---
+++ src/components/singer/singer.vue
@@ -0,0 +1,15 @@
+<template>
+  <!--<div class="singer" ref="singer">-->
+    <!--<list-view @select="selectSinger" :data="singers" ref="list"></list-view>-->
+    <!--<router-view></router-view>-->
+  <!--</div>-->
+  <div>si</div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
