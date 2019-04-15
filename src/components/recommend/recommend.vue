00186a5f7302f4177797b23accd745b701b0d57a
斌斌斌 <1587443838@qq.com>
2019-04-15 19:37:03 +0800

    loading组件的开发

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,35 +1,35 @@
 <template>
   <div class="recommend">
-      <scroll ref="scroll" class="recommend-content" :data="discList">
-        <div>
-          <div v-if="recommends.length" class="slider-wrapper">
-            <slider>
-              <div v-for="item in recommends">
-                 <a :href="item.linkUrl">
-                   <img @load="loadImage" class="needsclick" :src="item.picUrl">
-                 </a>
+    <scroll ref="scroll" class="recommend-content" :data="discList">
+      <div>
+        <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">
+          <slider>
+            <div v-for="item in recommends">
+              <a :href="item.linkUrl">
+                <img class="needsclick" @load="loadImage" :src="item.picUrl">
+              </a>
+            </div>
+          </slider>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li @click="selectItem(item)" v-for="item in discList" class="item">
+              <div class="icon">
+                <img width="60" height="60" v-lazy="item.picUrl">
               </div>
-            </slider>
-          </div>
-          <div class="recommend-list">
-            <h1 class="list-title">热门歌单推荐</h1>
-            <ul>
-              <li @click="selectItem(item)" v-for="item in discList" class="item">
-                <div class="icon">
-                  <img width="60" height="60" v-lazy="item.picUrl">
-                </div>
-                <div class="text">
-                  <h2 class="name" v-html="item.songListDesc"></h2>
-                  <p class="desc" v-html="item.accessnum"></p>
-                </div>
-              </li>
-            </ul>
-          </div>
+              <div class="text">
+                <h2 class="name" v-html="item.songListDesc"></h2>
+                <p class="desc" v-html="item.accessnum"></p>
+              </div>
+            </li>
+          </ul>
         </div>
-        <div class="loading-container" v-show="!discList.length">
-          <loading></loading>
-        </div>
-      </scroll>
+      </div>
+      <div class="loading-container" v-show="!discList.length">
+        <loading></loading>
+      </div>
+    </scroll>
   </div>
 </template>
 
@@ -49,7 +49,7 @@
     created(){
       setTimeout(()=>{
         this._getRecommend();
-      },3000)
+      },10)
       this._getDiscList();
     },
     methods:{
